1e2722ea218b3221ade101c68e24cf97a1317f8f
VitaliyBobyk <falcone159357bv@gmail.com>
2020-06-01 09:19:10 +0300

    uploaded UI of write article page

--- Django_blog/articles/forms.py
+++ Django_blog/articles/forms.py
@@ -6,3 +6,10 @@ class ArticleForm(forms.ModelForm):
     class Meta:
         model = Articles
         fields = ['title', 'post']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Title', 'class': 'col-md-7 form-control'}),
+            'post': forms.Textarea(attrs={'placeholder': 'Write you\'r Article!', 'class': 'col-md-7 form-control h-100 mt-3'})}
+
+
+
+
