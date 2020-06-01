9a0b4a930035e9cea8b874a5b1e336616a96d97e
VitaliyBobyk <falcone159357bv@gmail.com>
2020-06-01 16:09:10 +0300

    updated wrtie article page

--- Django_blog/articles/forms.py
+++ Django_blog/articles/forms.py
@@ -8,7 +8,8 @@ class ArticleForm(forms.ModelForm):
         fields = ['title', 'post']
         widgets = {
             'title': forms.TextInput(attrs={'placeholder': 'Title', 'class': 'col-md-7 form-control'}),
-            'post': forms.Textarea(attrs={'placeholder': 'Write you\'r Article!', 'class': 'col-md-7 form-control h-100 mt-3'})}
+            'post': forms.Textarea(attrs={'placeholder': 'Write you\'r Article!',
+                                          'class': 'col-md-7 form-control h-100 mt-3'})}
 
 
 
